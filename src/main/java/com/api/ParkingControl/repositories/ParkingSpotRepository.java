3d5b07f2af0b3885db18f503140d4f2b6275bc8b
Lukasnog18 <luksnogueira18@gmail.com>
2022-09-11 05:56:30 -0300

    Parking Control API

---
+++ src/main/java/com/api/ParkingControl/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package com.api.ParkingControl.repositories;
+
+import com.api.ParkingControl.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
---
+++ src/test/java/com/api/ParkingControl/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ParkingControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
